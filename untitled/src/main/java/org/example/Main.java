abc183cc76f2691f6cb6cededb0afc2beaa64272
Hung3032000 <phamngochung3032000@gmail.com>
2023-06-07 22:34:57 +0700

    Git: git 2

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Test Conflict");
+        System.out.println("Test Conflict2");
         System.out.println("feature-GIT-0");
     }
 }
